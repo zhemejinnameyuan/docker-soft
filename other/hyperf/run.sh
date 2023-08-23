f76fd5eff0ee59a7a748974dd255f74076555115
zhizhu <zhizhu@gmail.com>
2023-08-23 15:54:04 +0800

    add hyperf

--- other/hyperf/run.sh
+++ other/hyperf/run.sh
@@ -1,6 +1,8 @@
 docker run --name hyperf \
 -v /Users/zhu/www:/data/project \
 -p 9501:9501 -it \
+-p 9502:9502 -it \
+-p 9503:9503 -it \
 --privileged -u root \
 --entrypoint /bin/sh \
 hyperf/hyperf:8.0-alpine-v3.15-swoole
